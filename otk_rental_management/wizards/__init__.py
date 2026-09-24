2997d2bfb6c44b8bd8ceee03b94f53d4e41434c1
abgrxenda <abgr@xendagroup.com>
2026-09-24 15:21:07 +0300

    Initial commit: otk_rental_management for Odoo

---
+++ otk_rental_management/wizards/__init__.py
@@ -0,0 +1,7 @@
+from . import otk_rental_return_wizard
+from . import bulk_serial_wizard
+from . import serial_delete_confirm_wizard
+from . import serial_selection_wizard  # NEW - Add this line
+from . import otk_rental_pickup_wizard  # NEW
+from . import otk_rental_partial_return_wizard  # NEW
+from . import add_signature_wizard  # ADD THIS LINE
